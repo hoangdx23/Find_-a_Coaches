40f197cf388c159777f26253840463bce3a866b9
hoangdx <hoangdangxuan098@gmial.com>
2024-03-29 14:34:36 +0700

    first commit

---
+++ src/store/modules/coaches/actions.js
@@ -0,0 +1,57 @@
+export default {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
+    const coachData = {
+      firstName: data.first,
+      lastName: data.last,
+      description: data.desc,
+      hourlyRate: data.rate,
+      areas: data.areas
+    };
+
+    const response = await fetch(
+      `https://dem0-d122a-default-rtdb.asia-southeast1.firebasedatabase.app/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData)
+      }
+    );
+
+    if (!response.ok) {
+      const error = new Error('Failed to register coach!');
+      throw error;
+    }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId
+    });
+  },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://dem0-d122a-default-rtdb.asia-southeast1.firebasedatabase.app/coaches.json`
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error('Failed to load coaches!');
+      throw error;
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas
+      };
+      coaches.push(coach);
+    }
+
+    context.commit('setCoaches', coaches);
+  }
+};
